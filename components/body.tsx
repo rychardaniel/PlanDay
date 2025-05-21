923dfae6739b6799813f109d26be03a2a7cdcfd7
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2025-05-21 17:52:29 -0300

    scroll in calendar

--- components/body.tsx
+++ components/body.tsx
@@ -2,9 +2,22 @@ import { Calendar } from "./calendar";
 
 export function Body() {
     return (
-        <main className="bg-zinc-950 flex-1 flex justify-center p-4 sm:p-6">
-            <div className="bg-zinc-900 rounded-3xl flex-1 p-4">
-                <Calendar />
+        <main className="bg-zinc-950 h-[calc(100vh-68px)] flex justify-center">
+            <div className="bg-zinc-900 rounded-3xl w-full p-4 m-4 sm:m-6 overflow-hidden">
+                <div className="h-full overflow-y-auto pr-2 overflow-x-hidden">
+                    <Calendar />
+                    <div className="w-full">
+                        <ul className="flex flex-row ">
+                            <li className="flex justify-center w-1/7">D</li>
+                            <li className="flex justify-center w-1/7">S</li>
+                            <li className="flex justify-center w-1/7">T</li>
+                            <li className="flex justify-center w-1/7">Q</li>
+                            <li className="flex justify-center w-1/7">Q</li>
+                            <li className="flex justify-center w-1/7">S</li>
+                            <li className="flex justify-center w-1/7">S</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </main>
     );
