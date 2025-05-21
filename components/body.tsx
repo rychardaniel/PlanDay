1623e6ea7160bc0fa7584772b86f37ad9f3941d2
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2025-05-21 11:07:54 -0300

    fix padding

--- components/body.tsx
+++ components/body.tsx
@@ -1,8 +1,10 @@
+import { Calendar } from "./calendar";
+
 export function Body() {
     return (
-        <main className="bg-zinc-950 flex-1 flex justify-center p-8">
-            <div className="bg-zinc-900 rounded-3xl w-full flex-1 p-4">
-                ocupa 100 do main
+        <main className="bg-zinc-950 flex-1 flex justify-center p-4 sm:p-6">
+            <div className="bg-zinc-900 rounded-3xl flex-1 p-4">
+                <Calendar />
             </div>
         </main>
     );
