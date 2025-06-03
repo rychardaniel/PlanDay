c2bd1904999ce10a5df726e193fe27d2e5514d22
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2025-06-03 12:03:13 -0300

    refatora ModalAddEvent para melhorar a estrutura do código e a legibilidade, adiciona o input de nome de evento

--- components/modal-add-event.tsx
+++ components/modal-add-event.tsx
@@ -2,18 +2,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import { format } from "date-fns";
-
-const style = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
-    boxShadow: 24,
-    p: 4,
-};
+import TextField from "@mui/material/TextField";
 
 interface ModalAddEventsProps {
     open: boolean;
@@ -34,17 +23,43 @@ export function ModalAddEvent({
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                <Box sx={style}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
+                <Box
+                    sx={{
+                        position: "absolute",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%, -50%)",
+                        width: 400,
+                        bgcolor: "background.paper",
+                        border: "2px solid #000",
+                        boxShadow: 24,
+                        padding: 4,
+                        borderRadius: 4,
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: 2,
+                        }}
                     >
-                        Criar evento para {format(selectedDate, "dd/MM/yyyy")}
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        Em desenvolvimento
-                    </Typography>
+                        <Typography
+                            id="modal-modal-title"
+                            variant="h6"
+                            component="h2"
+                            sx={{ textAlign: "center" }}
+                        >
+                            Criar evento para{" "}
+                            {format(selectedDate, "dd/MM/yyyy")}
+                        </Typography>
+                        <TextField
+                            sx={{ width: "100%" }}
+                            id="outlined-basic"
+                            label="Nome do evento"
+                            variant="outlined"
+                        />
+                    </Box>
                 </Box>
             </Modal>
         </div>
