befb23b963a6af94bb43f715c23f7e64766ae632
rychardaniel <164958686+rychardaniel@users.noreply.github.com>
2025-06-12 10:09:03 -0300

    renomeia o arquivo LoadingScreen para loading-screen

--- context/LoadingContext.tsx
+++ context/LoadingContext.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useContext, useEffect, useState } from "react";
-import LoadingScreen from "@/components/LoadingScreen";
+import LoadingScreen from "@/components/loading-screen";
 
 const LoadingContext = createContext<{ loading: boolean }>({ loading: false });
 
